b82283ce09b747abd20712a9cbf52d64a473436f
caseyligas <39272507+caseyligas@users.noreply.github.com>
2018-05-14 10:37:07 -0500

    Add files via upload

---
+++ Export All Distribution Groups With Members.ps1
@@ -0,0 +1,5 @@
+﻿ForEach ($group in (Get-DistributionGroup -Filter {name -like "*"})) 
+   {  
+	$path = "C:\AllGroups\" + $group + ".csv"
+	Get-DistributionGroupMember $group | Select-Object firstname, lastname, primarysmtpaddress | Export-CSV -Path  $path
+   }
